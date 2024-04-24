0382973e7a79b1563de340f3b573ef2cc194d999
jasper99ww <kacperus4@onet.eu>
2024-04-24 14:31:52 +0200

    Applied event bubbling for improved event handling in todo list

--- src/app.js
+++ src/app.js
@@ -36,6 +36,7 @@ function createTodoItem(todo, todoIndex){
   const todoId = "todo-"+todoIndex;
   const todoLI = document.createElement("li");
   const todoText = todo.text;
+  todoLI.id = todoIndex;
   todoLI.className = "todo";
   todoLI.innerHTML = `
     <input type="checkbox" id="${todoId}">
@@ -46,30 +47,10 @@ function createTodoItem(todo, todoIndex){
       ${todoText}
     </label>
     <button class="delete-button">
-      <svg fill="var(--secondary-color)"xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
+      <svg fill="var(--secondary-color)" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
     </button>
   `
-  const deleteButton = todoLI.querySelector(".delete-button");
-  deleteButton.addEventListener("click", () => {
-    deleteTodoItem(todoIndex);
-  })
-  const checkbox = todoLI.querySelector("input");
-  checkbox.addEventListener("change", () => {
-    allTodos[todoIndex].completed = checkbox.checked;
-    saveTodos();
-  })
-  checkbox.checked = todo.completed;
   return todoLI;
-}
-function deleteTodoItem(todoIndex){
-  allTodos = allTodos.filter((_, i) => i !== todoIndex);
-  saveTodos();
-  updateTodoList();
-}
-
-function saveTodosToLocalStorage(){
-  const todosJson = JSON.stringify(allTodos)
-  localStorage.setItem("todos", todosJson);
 }
 
 function getTodos(){
@@ -77,3 +58,32 @@ function getTodos(){
   return JSON.parse(todos);
 }
 
+function saveTodos(){
+  const todosJson = JSON.stringify(allTodos)
+  localStorage.setItem("todos", todosJson);
+}
+
+function deleteTodoItem(todoIndex){
+  allTodos = allTodos.filter((_, i) => i !== todoIndex);
+  saveTodos();
+  updateTodoList();
+}
+
+todoListUL.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete-button")
+   || e.target.parentElement.classList.contains("delete-button")
+   || e.target.parentElement.parentElement.classList.contains("delete-button")) {
+    const todoId = e.target.closest("li").id;
+    const parsedTodoId = parseInt(todoId, 10);
+    deleteTodoItem(parsedTodoId);
+  }
+});
+
+todoListUL.addEventListener("change", (e) => {
+  if (e.target.type === "checkbox") {
+    const listItem = e.target.closest("li");
+    const todoIndex = parseInt(listItem.id.replace("todo-", ''), 10);
+    allTodos[todoIndex].completed = e.target.checked;
+    saveTodos();
+  }
+});
