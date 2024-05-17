32d9dee5f7fcb8c8fab783c0a746fc654f2a7fb7
jasper99ww <kacperus4@onet.eu>
2024-05-17 23:55:49 +0200

    Added visible overflow for toggle btn, overlay for main content on active sidebar, responsive layout for media query 768px

--- src/utils/SidebarHandler.js
+++ src/utils/SidebarHandler.js
@@ -21,18 +21,31 @@
 export class SidebarHandler {
   constructor() {
     this.sidebar = document.querySelector("#sidebar");
+    this.mainContainer = document.querySelector("#main-container"); // Referencja do głównego kontenera
     this.toggleBtn = document.querySelector("#toggle-sidebar-btn");
     this.init();
   }
 
   init() {
     this.toggleBtn.addEventListener('click', () => {
-      this.sidebar.classList.toggle('collapsed');
-      this.updateToggleIcon();
+      this.toggleSidebar();
     });
   }
 
+  toggleSidebar() {
+    this.sidebar.classList.toggle('collapsed');
+    if (!this.sidebar.classList.contains('collapsed') && window.innerWidth <= 768) {
+      // Dodanie efektu przyciemnienia tylko w trybie mobilnym
+      this.mainContainer.classList.add('active-overlay');
+    } else {
+      // Usunięcie efektu przyciemnienia
+      this.mainContainer.classList.remove('active-overlay');
+    }
+    this.updateToggleIcon();
+  }
+
   updateToggleIcon() {
+    // Zaktualizuj ikonę w zależności od stanu sidebara
     if (this.sidebar.classList.contains('collapsed')) {
       this.toggleBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>';
     } else {
